148db59a2914d15ab680fb859b9440b7717c7a51
Numan Abubakar <bhalonoman@gmail.com>
2023-09-08 16:52:45 +0500

    task1

---
+++ task1.ts
@@ -0,0 +1,14 @@
+// --------------------------------------Exercise 1------------------
+//Installing Done Node.js, TypeScript and VS Code on your computer.
+// ANS: INSTALLATION DONE!
+// --------------------------------------Exercise 2------------------
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let personName:string = "Numan"
+console.log(`AsslamAliakum ! ${personName},How are you?`);
+
+
+// --------------------------------------Exercise 3 ------------------
+
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
