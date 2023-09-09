f1f27960e8ce942b80f67e7e64d1e4899f987f79
Numan Abubakar <bhalonoman@gmail.com>
2023-09-09 15:02:01 +0500

    task 13

--- task1.js
+++ task1.js
@@ -57,3 +57,7 @@ for (let i = 0; i < friendsNames.length; i++) {
 for (let i = 0; i < friendsNames.length; i++) {
     console.log(`AsslamAliakum! ${friendsNames[i]} I Hope You are fine!`);
 }
+// -----------Exercise 13------------------
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+const frvtCars = ["Car", "Cycle", "Motorcycle"];
+frvtCars.forEach((item) => console.log(`I would like to own a ${item}`));
