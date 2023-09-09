4780067a635ea648ceaf25bb20d0effa5957195f
Numan Abubakar <bhalonoman@gmail.com>
2023-09-09 16:28:07 +0500

    task 16

--- task14to30.js
+++ task14to30.js
@@ -1,11 +1,18 @@
+"use strict";
 // -----------Exercise 14------------------
 //Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
-var guestList = ["Abdullah", "Ali", "Abdul Rehman", "Umar"];
-guestList.forEach(function (i) { return console.log("Im Inviting you for Dinner! ".concat(i, ".")); });
-// -----------Exercise 14------------------
+const guestList = ["Abdullah", "Ali", "Abdul Rehman", "Umar", "Abdul Wahab"];
+guestList.forEach((i) => console.log(`Im Inviting you for Dinner! ${i}.`));
+// -----------Exercise 15------------------
 //Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
-var notCome = "Ali";
-console.log(notCome);
+const notCome = "Ali";
+console.log("Not Make it", notCome);
 guestList.splice(1, 1, "Numan");
 // new list 
-guestList.forEach(function (i) { return console.log("Im Inviting you for Dinner! ".concat(i, ".")); });
+guestList.forEach((i) => console.log(`Im Inviting you for Dinner! ${i}.`));
+// -----------Exercise 16------------------
+// More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+console.log("founded a bigger dinner table");
+guestList.unshift("Awais");
+guestList.splice(guestList.length / 2, 0, "Umair");
+console.log("🚀 ~ file: task14to30.ts:30 ~ guestList:", guestList);
--- task14to30.ts
+++ task14to30.ts
@@ -2,12 +2,12 @@
 
   //Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
 
-  const guestList:string[]=["Abdullah","Ali","Abdul Rehman","Umar"]
+  const guestList:string[]=["Abdullah","Ali","Abdul Rehman","Umar","Abdul Wahab"]
   guestList.forEach((i)=>console.log(`Im Inviting you for Dinner! ${i}.`)
   )
 
 
-// -----------Exercise 14------------------
+// -----------Exercise 15------------------
 
   //Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
 
@@ -17,3 +17,19 @@ guestList.splice(1,1,"Numan")
 // new list 
 guestList.forEach((i)=>console.log(`Im Inviting you for Dinner! ${i}.`)
 )
+
+// -----------Exercise 16------------------
+
+// More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+
+console.log("founded a bigger dinner table");
+
+guestList.unshift("Awais")
+
+guestList.splice(guestList.length/2,0,"Umair")
+
+guestList.push("Abdul Manan")
+
+guestList.forEach((i)=>console.log(`Im Inviting you for Dinner! ${i}.`)
+)
+
