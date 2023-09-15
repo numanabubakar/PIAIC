6b2f1871c6715da82028300ac189ffb4a060ed3a
Numan Abubakar <bhalonoman@gmail.com>
2023-09-15 15:18:14 +0500

    27

--- task14to30.ts
+++ task14to30.ts
@@ -240,5 +240,28 @@ if(alien_color2 == 'green'){
 }
 
 
+// -----------Exercise 27------------------
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • If the alien is green, print a message that the player earned 5 points.
+
+// • If the alien is yellow, print a message that the player earned 10 points.
+
+// • If the alien is red, print a message that the player earned 15 points.
+
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+ if (alien_color2 == 'green') {
+  console.log("Player earned 5 points");
+  
+ }else if(alien_color2 == 'yellow'){
+  console.log("player earned 10 ponts");
+  
+ }else if(alien_color2 == 'red'){
+  console.log("player just earned 15 points");
+  
+ }else{
+  console.log("player earned 0 points");
+  
+ }
+
 
 
