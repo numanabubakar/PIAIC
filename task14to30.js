556c1aa13808b06c62d2192c0d348076ce1751d3
Numan Abubakar <bhalonoman@gmail.com>
2023-09-15 15:14:28 +0500

    tsc

--- task14to30.js
+++ task14to30.js
@@ -123,3 +123,13 @@ const searchItem = "orange";
 console.log(fruits.includes(searchItem)); // True
 const searchItem2 = "BMW";
 console.log(fruits.includes(searchItem)); // False
+// -----------Exercise 25------------------
+//Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+var alien_color = "green";
+if (alien_color === 'green') {
+    console.log('Congratulations! You just earned 5 points!');
+}
+alien_color = "red";
+if (alien_color === 'green') {
+    console.log('Congratulations! You just earned 5 points!');
+}
