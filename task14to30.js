e0f10df42d950c399a1dbbaaea85279e723bb06a
Numan Abubakar <bhalonoman@gmail.com>
2023-09-15 15:31:15 +0500

    30 comp

--- task14to30.js
+++ task14to30.js
@@ -205,3 +205,24 @@ else if (age == 20 || age < 65) {
 else {
     console.log("person is elder");
 }
+// -----------Exercise 29------------------
+/* Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+• Make a array of your three favorite fruits and call it favorite_fruits.
+
+• Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas! */
+let favorite_fruits = ["banana", "strawberry", "mango"];
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like bananas! ");
+}
+if (favorite_fruits.includes("strawberry")) {
+    console.log("You really like strawberries! ");
+}
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mangoes! ");
+}
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apples! ");
+}
+if (favorite_fruits.includes("watermelon")) {
+    console.log("You really like watermelons! ");
+}
